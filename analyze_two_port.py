d5a6b6c84a95e02ed5021d1d3996f53b52449274
Justin Hugon <jhugon@users.noreply.github.com>
2022-04-06 16:38:00 -0500

    working on two_port analysis

--- analyze_two_port.py
+++ analyze_two_port.py
@@ -5,6 +5,9 @@ import numpy as np
 import time
 import datetime
 import h5py
+from hist import Hist
+import matplotlib.pyplot as mpl
+import matplotlib.colors
 
 import vxi11
 
@@ -38,6 +41,7 @@ def collect_step_response_data(ip,verts_in,verts_sig_gen,nWaveforms,in_channel="
 
     return out_file_name
 
+
 def collect_positive_step_response_data(ip,amplitudes_sig_gen,nWaveforms,in_channel="channel1",sig_gen_channel="1",horiz=(500e-9,0)):
     verts_sig_gen = [(x,0.5*x) for x in amplitudes_sig_gen]
     scale_in = find_smallest_setting(np.array(amplitudes_sig_gen)*1.1/4.)
@@ -52,7 +56,33 @@ def collect_bipolar_step_response_data(ip,amplitudes_sig_gen,nWaveforms,in_chann
     verts_in = [(x,0.) for x in scale_in]
     return collect_step_response_data(ip,verts_in,verts_sig_gen,nWaveforms,in_channel=in_channel,sig_gen_channel=sig_gen_channel,horiz=horiz)
 
+
+def analyze_step_response_data(fn):
+
+    with h5py.File(fn) as f:
+        sr_dir = f["step_response"]
+        sig_gen_Vpp_values = []
+        for sgs_key in sr_dir:
+            sgs_dir = sr_dir[sgs_key]
+            sig_gen_Vpp = sgs_dir.attrs["amplitude"]
+            sig_gen_Vpp_values.append(sig_gen_Vpp)
+        print(sig_gen_Vpp_values)
+        waveform_hist = Hist.new.Variable(sig_gen_Vpp_values+[sig_gen_Vpp_values[-1]*2],name="sig_gen_Vpp",label="Signal Generator $V_{pp}$ [V]").Reg(500,-0.6,0.6,name="waveform",label="Waveform [V]").Double()
+        for sgs_key in sr_dir:
+            sgs_dir = sr_dir[sgs_key]
+            sig_gen_Vpp = sgs_dir.attrs["amplitude"]
+            waveforms_dset = sgs_dir["waveforms"]
+            ts = waveforms_dset.dims[1][0]
+            waveform_hist.fill(sig_gen_Vpp,waveforms_dset[:,:].flatten())
+        fig, ax = mpl.subplots(figsize=(6,6),constrained_layout=False)
+        waveform_hist.plot2d(ax=ax,norm=matplotlib.colors.PowerNorm(gamma=0.3))#,vmax=5000))
+        ax.set_title("Recorded Waveform Values")
+        fig.savefig("step_response_waveform_hist.png")
+        fig.savefig("step_response_waveform_hist.pdf")
+        
+
 if __name__ == "__main__":
     ip = "192.168.55.2"
-    #out_fn = collect_positive_step_response_data(ip,[0.01,0.03,0.05,0.1,0.3,0.5],5)
-    out_fn = collect_positive_step_response_data(ip,[0.1,0.3,0.5],2)
+    #out_fn = collect_positive_step_response_data(ip,[0.01,0.03,0.05,0.1,0.3,0.5],20)
+    fn = "step_response_2022-04-06T16:07:16_20waveforms.hdf5"
+    analyze_step_response_data(fn)
