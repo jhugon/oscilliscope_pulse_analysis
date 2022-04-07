12b9441f2ebcd8094e897a68c44c68d8de344b9b
Justin Hugon <jhugon@users.noreply.github.com>
2022-04-07 13:57:54 -0500

    Done with step response for now

--- analyze_two_port.py
+++ analyze_two_port.py
@@ -77,23 +77,42 @@ def find_waveform_bottom_top(waveform_hist):
             return [None,None]
         return results
 
+def find_step_times(profile,bottom,mid,top):
+    # clip 10% on either end
+    nBinsOrig = profile.size
+    profile = profile[int(0.1*nBinsOrig):int(0.9*nBinsOrig)]
+    Vpp = top-bottom
+    V1pct = Vpp*0.01+bottom
+    V10pct = Vpp*0.1+bottom
+    V90pct = Vpp*0.9+bottom
+    V99pct = Vpp*0.99+bottom
+    tMid = profile.axes[0].centers[profile.values() >= mid][0]
+    profile_before_mid = profile[:tMid*1j]
+    profile_after_mid = profile[tMid*1j:]
+    t1pct = profile_before_mid.axes[0].centers[profile_before_mid.values() <= V1pct][-1]
+    t10pct = profile_before_mid.axes[0].centers[profile_before_mid.values() <= V10pct][-1]
+    t90pct = profile_after_mid.axes[0].centers[profile_after_mid.values() >= V90pct][0]
+    t99pct = profile_after_mid.axes[0].centers[profile_after_mid.values() >= V99pct][0]
+    tSettle1pct = profile_after_mid.axes[0].centers[abs(profile_after_mid.values()-top)/(top-bottom) > 0.01][-1]
+    tSettle0p1pct = profile_after_mid.axes[0].centers[abs(profile_after_mid.values()-top)/(top-bottom) > 0.005][-1]
+    return tMid, t1pct, t10pct, t90pct, t99pct, tSettle1pct, tSettle0p1pct
+
 def analyze_step_waveform_dset(waveform_dset,sig_gen_Vpp):
 
         caption = f"Sig-Gen Vpp = {sig_gen_Vpp}"
-        waveform_hist = make_hist_waveformVtime(waveform_dset,time_units="ns",voltage_units="mV",downsample_time_by=10)
+        waveform_hist = make_hist_waveformVtime(waveform_dset,time_units="s",voltage_units="V",downsample_time_by=1)
         bottom,top = find_waveform_bottom_top(waveform_hist)
         Vpp = top-bottom
         mid = (top+bottom)/2.
 
         waveform_profile = waveform_hist.profile("voltage")
-        waveform_spline = interpolate.UnivariateSpline(waveform_profile.axes[0].edges[:-1],waveform_profile.values(),k=2,s=0)
 
         Vmax = max(waveform_profile.values())
         Vmin = min(waveform_profile.values())
 
         overshoot = (Vmax-top)/Vpp
         undershoot = (bottom-Vmin)/Vpp
-        t50 = 0.
+        tMid, t1pct, t10pct, t90pct, t99pct, tSettle1pct, tSettle0p1pct = find_step_times(waveform_profile,bottom,mid,top)
 
         statistics = {
             "top": top,
@@ -104,24 +123,31 @@ def analyze_step_waveform_dset(waveform_dset,sig_gen_Vpp):
             "min" : Vmin,
             "overshoot": overshoot,
             "undershoot": undershoot,
-            "t50": t50,
+            "tMid": tMid,
+            "t1pct": t1pct,
+            "t10pct": t10pct,
+            "t90pct": t90pct,
+            "t99pct": t99pct,
+            "tSettle1pct": tSettle1pct,
+            "tSettle0p1pct" : tSettle0p1pct,
+            "risetime10-90" : t90pct-t10pct,
+            "risetime1-99" : t99pct-t1pct,
         }
-        print(caption)
-        print(statistics)
 
         fig, ax = mpl.subplots(figsize=(6,6),constrained_layout=False)
         waveform_hist.plot2d(ax=ax,norm=PHOSPHOR_HIST_NORM)
+        ax.axvline(t10pct,c="0.5")
+        ax.axvline(t90pct,c="0.5")
+        ax.axvline(tSettle1pct,c="0")
+        ax.axvline(tSettle0p1pct,c="0")
         ax.axhline(bottom,c="0.5")
         ax.axhline(top,c="0.5")
         waveform_profile.plot(ax=ax,color="r")
-        x = np.linspace(-20,40)
-        ax.plot(x,waveform_spline(x))
-        ax.set_xlim(-20,40)
-        #ax.set_ylim(-5,15)
+        ax.set_xlim(t1pct-1*(t99pct-t1pct),t99pct+5*(t99pct-t1pct))
+        ax.set_ylim(Vmin-0.1*Vpp,Vmax+0.1*Vpp)
         fig.suptitle(caption)
         fig.savefig(f"step_response_waveform_{sig_gen_Vpp}.png")
-
-
+        return statistics
 
 
 def analyze_step_response_data(fn):
@@ -129,16 +155,27 @@ def analyze_step_response_data(fn):
     with h5py.File(fn) as f:
         sr_dir = f["step_response"]
         sig_gen_Vpp_values = []
-        waveform_hists = []
+        stats = []
         for sgs_key in sr_dir:
             sgs_dir = sr_dir[sgs_key]
             sig_gen_Vpp = sgs_dir.attrs["amplitude"]
             sig_gen_Vpp_values.append(sig_gen_Vpp)
-            analyze_step_waveform_dset(sgs_dir["waveforms_raw"],sig_gen_Vpp)
-        
+            stat = analyze_step_waveform_dset(sgs_dir["waveforms_raw"],sig_gen_Vpp)
+            stats.append(stat)
+        fig, ax = mpl.subplots(figsize=(6,6),constrained_layout=True)
+        ax.plot(sig_gen_Vpp_values,[x["Vpp"] for x in stats])
+        ax.set_xlabel("Signal Generator Amplitude [V]")
+        ax.set_ylabel("V$_{pp}$ [V]")
+        fig.savefig("step_response_Vpp.png")
+        fig, ax = mpl.subplots(figsize=(6,6),constrained_layout=True)
+        ax.plot(sig_gen_Vpp_values,[x["risetime1-99"] for x in stats],label="1%-99%")
+        ax.plot(sig_gen_Vpp_values,[x["risetime10-90"] for x in stats],label="10%-90%")
+        ax.set_xlabel("Signal Generator Amplitude [V]")
+        ax.set_ylabel("Rise Time [s]")
+        fig.savefig("step_response_rise_time.png")
+ 
 
 if __name__ == "__main__":
     ip = "192.168.55.2"
-    #fn = collect_positive_step_response_data(ip,[0.01,0.03,0.05,0.1,0.3,0.5],20)
-    fn = "step_response_2022-04-07T10:26:50_20waveforms.hdf5"
+    fn = collect_positive_step_response_data(ip,[0.01,0.03,0.05,0.1,0.3,0.5],20)
     analyze_step_response_data(fn)
